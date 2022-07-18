ce62465d3afc64a8f4b701d6c9294ebe16e975b3
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:29:28 +0530

    added-routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+  return;
+  <BrowserRouter>
+    <Routes>
+      <Route path="" element={<h1>homepage</h1>} />
+      <Route path="login" element={<h1>login</h1>} />
+      <Route path="signup" element={<h1>signup</h1>} />
+      <Route path="cart" element={<h1>cart</h1>} />
+      <Route path="orders" element={<h1>orders</h1>} />
+      <Route path="profile" element={<h1>profile</h1>} />
+      <Route path="errorpage" element={<h1>errorpage</h1>} />
+    </Routes>
+  </BrowserRouter>;
 }
